9c638fb6b22d56c7869ef8b5eb2f6fcc75c1a925
Thakur Karan Singh <karansingh.thkur@gmail.com>
2023-11-30 08:25:31 -0500

    all changes

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
-export const BASE_URL = `http://localhost:${process.env.PORT || 3000}`;
-export const API_URL = `http://localhost:${process.env.PORT || 3003}/api/`;
-export const PRESET_KEY = "chat-app";
-export const CLOUDINARY_URL = "https://api.cloudinary.com/v1_1/dpo25fsfu/image/upload";
-export const CLOUD_NAME = "dpo25fsfu";
+require('dotenv').config();
+export const BASE_URL ="http://localhost:3000";
+export const API_URL = "http://localhost:3006/api";
+
+
--- src/utils/imageUpload.js
+++ src/utils/imageUpload.js
@@ -1,49 +1,45 @@
-import { CLOUD_NAME, PRESET_KEY, CLOUDINARY_URL } from '../utils/config';
-
 export const checkImage = (file) => {
-  if (!file) {
-    throw new Error("File does not exist.");
-  }
+    let err = "";
+    if(!file){
+        return err = "File does not exist.";
+    }
+//?1 mb
+    if(file.size > 1024 * 1024){
+         return (err = "File size must be less than 1 Mb.");
+    }
 
-  // 1 MB
-  if (file.size > 1024 * 1024) {
-    throw new Error("File size must be less than 1 Mb.");
-  }
+    if (file.type !== 'image/jpeg' && file.type !== 'image/png') {
+      return (err = "Image must be jpeg or png.");
+    }
 
-  if (file.type !== 'image/jpeg' && file.type !== 'image/png') {
-    throw new Error("Image must be jpeg or png.");
-  }
-};
+    return err;
+}
+
 
 export const imageUpload = async (images) => {
-  let imgArr = [];
+    let imgArr = [];
+    for(const item of images){
+        const formData = new FormData();
 
-  for (const item of images) {
-    try {
-      checkImage(item);
+        if(item.camera){
+            formData.append("file", item.camera);
+        }else{
+            formData.append("file", item);  
+        }
 
-      const formData = new FormData();
-      formData.append("file", item.camera || item);
-      formData.append("upload_preset", PRESET_KEY);
-      formData.append("cloud_name", CLOUD_NAME);
+        
+        formData.append("upload_preset", "chat-app");
+        formData.append("cloud_name", "dpo25fsfu");
 
-      const res = await fetch(CLOUDINARY_URL, {
-        method: "POST",
-        body: formData,
-      });
+        const res = await fetch("https://api.cloudinary.com/v1_1/dpo25fsfu/image/upload", {
+            method: "POST",
+            body: formData
+        })
 
-      if (!res.ok) {
-        throw new Error(`Image upload failed with status ${res.status}`);
-      }
-
-      const data = await res.json();
-      imgArr.push({ public_id: data.public_id, url: data.secure_url });
-    } catch (error) {
-      console.error("Image upload error:", error);
-      // Handle the error as needed, e.g., show a user-friendly message
+        const data = await res.json();
+        imgArr.push({ public_id: data.public_id, url: data.secure_url });
+        
+      
     }
-  }
-
-  return imgArr;
-};
-
+    return imgArr;
+}
